bae1f613b2cef0bb28f43712f46469929751dc34
Zheka Grem <odinked@gmail.com>
2024-08-08 12:30:34 +0300

    change metadata

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -13,9 +13,7 @@ export const siteMetadata: Metadata = {
     title: 'FWOG',
     description:
       'In the ashes a community emerged, a new flog, a more based flog, a FWOG. FWOG has no dev. It is the community.',
-    images: [
-      'https://as1.ftcdn.net/v2/jpg/04/54/10/86/1000_F_454108632_C48wbroFTZQQR5tiSeg0oX0GkrYE8j8Z.jpg',
-    ],
+    images: ['https://frog-steel.vercel.app/_next/static/media/1.c855e518.webp'],
 
     type: 'website',
   },
@@ -23,9 +21,7 @@ export const siteMetadata: Metadata = {
     title: 'FWOG',
     description:
       'In the ashes a community emerged, a new flog, a more based flog, a FWOG. FWOG has no dev. It is the community.',
-    images: [
-      'https://as1.ftcdn.net/v2/jpg/04/54/10/86/1000_F_454108632_C48wbroFTZQQR5tiSeg0oX0GkrYE8j8Z.jpg',
-    ],
+    images: ['https://frog-steel.vercel.app/_next/static/media/1.c855e518.webp'],
   },
   robots: 'index, follow',
   alternates: {
