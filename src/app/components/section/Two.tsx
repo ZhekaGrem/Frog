2bc8f7f86e72ff81b20577d4d9023b99aef4e9d7
Zheka Grem <odinked@gmail.com>
2024-08-08 12:28:13 +0300

    change text

--- src/app/components/section/Two.tsx
+++ src/app/components/section/Two.tsx
@@ -7,7 +7,7 @@ const Two = () => {
   return (
     <section className={`${fredoka.className} img2`}>
       <div className="container mx-auto text-center text-white">
-        <h2 className="second-text mb-8 text-2xl font-semibold">ABOUT FWOG</h2>
+        <h2 className="second-text mb-8 text-2xl font-semibold">Костя Грачов</h2>
         <p className="second-text m-auto max-w-screen-xl text-3xl font-normal">
           In the ashes a community emerged, a new flog, a more based flog, a FWOG. FWOG has no dev. It is the
           community.
